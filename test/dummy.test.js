9711232c82e244d4f0a9f9f72bc008f8d76eaf8c
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2020-11-02 17:45:00 +0200

    4.6 tehty

--- test/dummy.test.js
+++ test/dummy.test.js
@@ -149,3 +149,106 @@ describe('favorite blog', () => {
         expect(result).toEqual(listWithSameLikes[0])
     })
 })
+
+describe('most common author', () => {
+    const listOfAuthors = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '1978578926798qa',
+            title: 'Blog for testing number 678',
+            author: 'Johnny Testman',
+            url: 'http://www.google.com',
+            likes: 21,
+            __v: 0
+        },
+        {
+            _id: '5490876889afsag',
+            title: 'Blog about nice flowers',
+            author: 'Matti Meikäläinen',
+            url: 'http://www.w3schools.com',
+            likes: 2,
+            __v: 0
+        },
+        {
+            _id: '489788979876a7fh',
+            title: 'Last blog about cool things',
+            author: 'Matti Meikäläinen',
+            url: 'http://www.iltalehti.fi',
+            likes: 13,
+            __v: 0
+        }
+    ]
+
+    const listOfSameAuthors = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '1978578926798qa',
+            title: 'Blog for testing number 678',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.google.com',
+            likes: 21,
+            __v: 0
+        },
+        {
+            _id: '5490876889afsag',
+            title: 'Blog about nice flowers',
+            author: 'Matti Meikäläinen',
+            url: 'http://www.w3schools.com',
+            likes: 2,
+            __v: 0
+        },
+        {
+            _id: '489788979876a7fh',
+            title: 'Last blog about cool things',
+            author: 'Matti Meikäläinen',
+            url: 'http://www.iltalehti.fi',
+            likes: 13,
+            __v: 0
+        }
+    ]
+
+    const onlyOneAuthor = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('of empty list is none', () => {
+        const result = listHelper.mostBlogs([])
+        expect(result).toEqual({})
+    })
+
+    test('of list with one entry is Edsger W. Dijkstra', () => {
+        const result = listHelper.mostBlogs(onlyOneAuthor)
+        expect(result).toEqual({ author: 'Edsger W. Dijkstra', blogs: 1 })
+    })
+
+    test('of list is Matti Meikäläinen', () => {
+        const result = listHelper.mostBlogs(listOfAuthors)
+        expect(result).toEqual({ author: 'Matti Meikäläinen', blogs: 2 })
+    })
+
+    test('of list with multiple authors with same amount is Edsger W. Dijkstra', () => {
+        const result = listHelper.mostBlogs(listOfSameAuthors)
+        expect(result).toEqual({ author: 'Edsger W. Dijkstra', blogs: 2 })
+    })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+var lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -30,8 +32,48 @@ const favoriteBlog = (blogs) => {
     return favorite
 }
 
+const mostBlogs = (blogs) => {
+
+    if (blogs.length == 0) {
+        return {}
+    }
+
+    var index = 0
+    var blogAuthors = []
+
+    //Otetaan ylös kaikki authorit (myös samat)
+    blogs.forEach(blog => {
+        blogAuthors[index] = blog.author
+        index++
+    });
+
+    //Lasketaan authorien esiintyvyydet
+    const result = lodash.values(lodash.groupBy(blogAuthors)).map(d => ({ name: d[0], count: d.length }));
+
+    //Katsotaan mitä löytyy eniten
+    var biggestCount =
+        Math.max.apply(Math, result.map(function (f) {
+            return f.count
+        }))
+
+    var favorite =
+        result.find(function (f) {
+            return f.count == biggestCount
+        })
+
+    //Muutetaan vielä objektin muotoa
+    var author =
+    {
+        author: favorite.name,
+        blogs: favorite.count
+    }
+
+    return author
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
